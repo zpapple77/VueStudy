42b92ddc5b0e85a18c2e621c033fdcfed2d6a32b
Pinger <zpapple77@163.com>
2022-06-12 16:04:35 +0800

    🎉 init(配置文件): 端口修改

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue'//引入vue对象，类似于<script src="vue.js"></script>
+import App from './App.vue'//App.vue文件对象引入过来（vue项目的入口）
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false//控制台有一句提示信息
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+new Vue({//开始实例化cue
+  render: h => h(App),//转变渲染App页面
+}).$mount('#app')//渲染index.html到id叫app的标签上
