976479363be8d30eb86fe2e7814d7d5a37de2d16
Pinger <zpapple77@163.com>
2022-06-12 23:25:59 +0800

    ✨ feat(vue.app): 插值表达式

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!-- 2.把值赋予到标签 -->
+    <h1>{{msg}}</h1>
+    <h2>{{obj.name}}</h2>
+    <h3>{{age>18?"成年":"未成年"}}</h3>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  //1.变量在data函数return的对象上
+  data(){
+    return{
+      msg:'hello,zhang',
+      obj:{
+        name:'小vue',
+        age:5
+      }
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,6 @@ module.exports = defineConfig({
   devServer:{
     port:3000,
     open:true//浏览器自动打开
-  }
+  },
+  lintOnSave:false//关闭eslint检查
 })
