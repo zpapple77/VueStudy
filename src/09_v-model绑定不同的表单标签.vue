0c25f0f260bf543aa4ae47d4afafe752bf4fb433
Pinger <zpapple77@163.com>
2022-06-20 23:30:10 +0800

    commit

---
+++ src/09_v-model绑定不同的表单标签.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <!-- 
+    	v-model:是实现vuejs变量和表单标签value属性, 双向绑定的指令
+    -->
+    <div>
+      <span>用户名：</span>
+      <input type="text" v-model="username" />
+    </div>
+    <div>
+      <span>密码:</span>
+      <input type="password" v-model="pass" />
+    </div>
+    <div>
+      <span>来自于：</span>
+      <!--下拉框要绑定在select上 -->
+      <select v-model="from">
+        <option value="beijing">北京</option>
+        <option value="nanjing">南京</option>
+        <option value="wuhan">武汉</option>
+      </select>
+    </div>
+    <div>
+      <span>爱好：</span>
+      <!-- (重要)
+      遇到复选框, v-model的变量值
+      非数组 - 关联的是复选框的checked属性
+      数组   - 关联的是复选框的value属性
+       -->
+      <input type="checkbox" v-model="hobby" value="抽烟" />抽烟
+      <input type="checkbox" v-model="hobby" value="喝酒" />喝酒
+      <input type="checkbox" v-model="hobby" value="写代码" />写代码
+    </div>
+    <div>
+      <span>性别：</span>
+      <input type="radio" v-model="gender" value="男" name="sex" />男
+      <input type="radio" v-model="gender" value="女" name="sex" />女
+    </div>
+    <div>
+      <span>自我介绍</span>
+      <textarea v-model="intro"></textarea>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      pass: '',
+      from: '',
+      hobby: [],
+      gender: '',
+      intro: '',
+    }
+    // 总结:
+    // 特别注意: v-model, 在input[checkbox]的多选框状态
+    // 变量为非数组, 则绑定的是checked的属性(true/false) - 常用于: 单个绑定使用
+    // 变量为数组, 则绑定的是他们的value属性里的值 - 常用于: 收集勾选了哪些值
+  },
+}
+</script>
+
+<style></style>
---
+++ src/10_v-html&v-text.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <!-- v-text把值当成普通字符串显示, v-html把值当做html解析 -->
+    <p v-text="str"></p>
+    <p v-html="str">{{10+20}}</p>
+    <!-- v-text/html会覆盖插值表达式 -->
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      str: '<span>hello</span>'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
